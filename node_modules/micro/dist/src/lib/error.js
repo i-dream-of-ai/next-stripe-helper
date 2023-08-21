01849fec30c9c6442aae5d780e85405541e1f376
J Hanlon <kitchenbeats@gmail.com>
2023-08-21 15:16:31 -0700

    remove gitignore files

--- node_modules/micro/dist/src/lib/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logError = void 0;
-function logError(message, errorCode) {
-    console.error(`micro: ${message}`);
-    console.error(`micro: https://err.sh/micro/${errorCode}`);
-}
-exports.logError = logError;
